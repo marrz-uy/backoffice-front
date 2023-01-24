357984244aa783e586e2bbc70a82f8150e71fc4f
jromero764 <jromeroprogramador14@gmail.com>
2023-01-23 22:19:42 -0300

    Gestion de Tour v1

--- Tour.js
+++ Tour.js
@@ -107,10 +107,9 @@ function ConsultarPuntosDeInteresParaTour(categoria) {
         $('#tbody').append(`<tr class="table-active">
         <th scope="row">${js[i].Nombre}</th>
         <td>${js[i].Departamento}</td>
-        <td>${js[i].Ciudad}</td>
-        <td>${js[i].Direccion}</td>
+       
         <td>
-              <svg onclick="getDataTour('${js[i].id}','${js[i].Nombre}','${js[i].Direccion}');" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 16 16">
+              <svg onclick="getDataTour('${js[i].id}','${js[i].Nombre}','${js[i].Departamento}','${js[i].Direccion}');" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 16 16">
                   <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z"/>
               </svg>
         </td>
@@ -122,10 +121,11 @@ function ConsultarPuntosDeInteresParaTour(categoria) {
     
   }).fail(function (jqXHR, textStatus, errorThrown) {ErrorHandler(jqXHR, textStatus);});
 }
-function getDataTour(id,Nombre,Direccion) {
+function getDataTour(id,Nombre,Departamento,Direccion) {
 $('#tbody-tourPreview').append(`
 <tr class="table-active">
 <th scope="row">${Nombre}</th>
+<td>'${Departamento}'</td>
 <td>'${Direccion}'</td>
 <td><i onclick="EliminarEvento(${id});" class="bi bi-trash pointer" ></i></td>
 </tr>
@@ -139,6 +139,7 @@ function getInputTour(){
     descripcionTourPredefinido:$('#descripcionTourPredefinido').val()
   };
   console.log(InformacionTour);
+  $('#titulo-tabla-tour').text(`Preview - ${$('#nombreTourPredefinido').val()}`);
   return InformacionTour;
 }
 function AltaDeTour(){
