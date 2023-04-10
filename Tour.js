c87111b980e01215f71617cd6b5817fff9efbe1a
unknown <Javi>
2023-04-10 19:07:00 -0300

    lo ultimo

--- Tour.js
+++ Tour.js
@@ -183,7 +183,7 @@ function ConsultaTourHtml() {
       <th scope="col">Nombre del Tour</th>
       <th scope="col">Hora de Inicio</th>
       <th scope="col">Descripcion</th>  
-      <th style="text-align: right;">
+      <th class="text-center" style="text-align: right;">
           <button type="button" class="btn btn-success">
               <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
                   <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"></path>
