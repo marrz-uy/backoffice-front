b5707b71f5c676e6bd13da904f8d759ae3d83f1e
jromero764 <jromeroprogramador14@gmail.com>
2023-04-25 21:15:31 -0300

    arreglos de eventos

--- Eventos.js
+++ Eventos.js
@@ -82,12 +82,12 @@ function mostrarPunto(datos){
   $('#tbody').html('');
   for(i=0;i<datos.length;i++){
     $('#tbody').append(`<tr class="table-active">
-          <th scope="row">${datos[i].Nombre}</th>
-          <td>${datos[i].Departamento}</td>
-          <td>${datos[i].Ciudad}</td>
-          <td>${datos[i].Direccion}</td>
+          <th scope="row">${datos[i][0].Nombre}</th>
+          <td>${datos[i][0].Departamento}</td>
+          <td>${datos[i][0].Ciudad}</td>
+          <td>${datos[i][0].Direccion}</td>
           <td class="text-center">
-                <svg onclick="getInputLugarDelEvento('${datos[i].id}','${datos[i].Nombre}');" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 16 16">
+                <svg onclick="getInputLugarDelEvento('${datos[i][0].id}','${datos[i][0].Nombre}');" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 16 16">
                     <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z"/>
                 </svg>
           </td>
